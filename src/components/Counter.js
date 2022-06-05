9e1d3639939f05ee627422d32ec190be3aed8ffc
mesu-ai <mesu.s.ss@gmail.com>
2022-06-06 03:14:22 +0600

    react-redux

---
+++ src/components/Counter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Counter = () => {
+    return (
+        <div>
+            <p>0</p>
+
+            <div>
+                <button>+</button>
+                <button>Reset</button>
+                <button>-</button>
+            </div>
+
+
+            
+        </div>
+    );
+};
+
+export default Counter;
